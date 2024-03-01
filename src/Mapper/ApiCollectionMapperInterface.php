04d76ba69046bd5f339e04997e66d13727d114f0
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-03-01 19:04:20 +0700

    refactor: Move `mapCollection()` to separate interface.

---
+++ src/Mapper/ApiCollectionMapperInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of rekalogika/api-lite package.
+ *
+ * (c) Priyadi Iman Nurcahyo <https://rekalogika.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
+ */
+
+namespace Rekalogika\ApiLite\Mapper;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\Pagination\PaginatorInterface;
+use Rekalogika\Mapper\Context\Context;
+
+interface ApiCollectionMapperInterface
+{
+    /**
+     * @template TOutput of object
+     * @param class-string<TOutput> $target
+     * @param array<string,mixed> $context
+     * @return PaginatorInterface<TOutput>
+     */
+    public function mapCollection(
+        object $collection,
+        string $target,
+        Operation $operation,
+        array $context = [],
+        ?Context $mapperContext = null,
+    ): PaginatorInterface;
+}
