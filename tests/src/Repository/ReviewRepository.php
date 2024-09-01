dbeb0661b745c6a04ead3f9766c37418864ac875
Priyadi Iman Nurcahyo <1102197+priyadi@users.noreply.github.com>
2024-09-01 16:34:15 +0700

    deps: bump rekapager

--- tests/src/Repository/ReviewRepository.php
+++ tests/src/Repository/ReviewRepository.php
@@ -12,9 +12,9 @@ use Doctrine\Persistence\ManagerRegistry;
  * @extends ServiceEntityRepository<Review>
  *
  * @method Review|null find($id, $lockMode = null, $lockVersion = null)
- * @method Review|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Review|null findOneBy(array<string,mixed> $criteria, ?array<string,string> $orderBy = null)
  * @method Review[]    findAll()
- * @method Review[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Review[]    findBy(array<string,mixed> $criteria, ?array<string,string> $orderBy = null, $limit = null, $offset = null)
  */
 class ReviewRepository extends ServiceEntityRepository
 {
